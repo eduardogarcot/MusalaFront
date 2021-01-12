83c54f0f7c9a3041c78281200efc438e1feedfb4
eduardogarcot <eduardogarcot@gmail.com>
2021-01-12 01:34:15 -0500

    making-table-a-component-reutilizable

--- src/components/gateways.jsx
+++ src/components/gateways.jsx
@@ -4,17 +4,21 @@ import Table from "./table";
 class Gateways extends Component {
 
     render() {
-        let items=[
-            {key:0, label:"ID Serial Number"},
-            {key:1, label:"Name"},
-            {key:2, label:"IP Address"},
-            {key:3, label:"Devices Connected"}
+        let labels=[
+            {path: "_id", label:"ID Serial Number"},
+            {path:"name", label:"Name"},
+            {path:"ipaddress", label:"IP Address"},
+            {path:"peripheralDevices", label:"Devices Connected"}
         ];
-        {/**/}
-
+        let gateways= [
+            {_id:1, name:"Felipe", ipaddress:"10.8.91.145", peripheralDevices:5},
+            {_id:2, name:"Carlos", ipaddress:"10.8.200.132", peripheralDevices:2},
+            {_id:3, name:"Antonio", ipaddress:"10.8.10.56", peripheralDevices:2},
+            {_id:4, name:"Eduardo", ipaddress:"10.8.56.15", peripheralDevices:3},
+        ]
         return (
             <div>
-                <Table itemsLabels={items}/>
+                <Table columns={labels} data={gateways} />
             </div>
         );
     }
--- src/components/peripheraldevices.jsx
+++ src/components/peripheraldevices.jsx
@@ -3,18 +3,25 @@ import Table from "./table";
 
 class PeripheralDevices extends Component {
     render() {
-        let items=[
-            {key:0, label:"Device ID"},
-            {key:1, label:"Vendor"},
-            {key:2, label:"Created Date"},
-            {key:3, label:"Is Online"},
-            {key:4, label:"Connected to (Gateway ID)"}
+        let labels=[
+            {path: "_id", label:"Device Id"},
+            {path:"vendor", label:"vendor"},
+            {path:"createdDate", label:"Created Date"},
+            {path:"onlineStatus", label:"Is Online", content:gateway=>{
+                if (gateway.onlineStatus) return <p>Yes</p>;
+                    return <p>No</p>
+                }},
+            {path:"serialNumberId", label:"Gateway ID"}
         ];
-        {/**/}
-
+        let gateways= [
+            {_id:10, vendor:"Felipe", createdDate:"10.8.91.145", onlineStatus:true, serialNumberId:1},
+            {_id:20, vendor:"Carlos", createdDate:"10.8.200.132", onlineStatus:false, serialNumberId:1},
+            {_id:30, vendor:"Antonio", createdDate:"10.8.10.56", onlineStatus:false, serialNumberId:2},
+            {_id:40, vendor:"Eduardo", createdDate:"10.8.56.15", onlineStatus:true, serialNumberId:1},
+        ];
         return (
             <div>
-                <Table itemsLabels={items}/>
+                <Table columns={labels} data={gateways} />
             </div>
         );
     }
