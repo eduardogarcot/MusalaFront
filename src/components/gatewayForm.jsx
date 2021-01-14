9793af7d6480fb0830341be9e3a30b2767d162e9
eduardogarcot <eduardogarcot@gmail.com>
2021-01-14 14:41:58 -0500

    making-gateways-form-without-saving-changes

--- src/components/gatewayForm.jsx
+++ src/components/gatewayForm.jsx
@@ -1,11 +1,65 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Joi from "joi-browser";
+import Form from "./form";
 
-class GatewayForm extends Component {
+class GatewayForm extends Form {
+    state = {
+        data: {
+            _id:"",
+            name:"",
+            ipAddress:"",
+            devicesList:"",
+        },
+        errors:{}
+    };
+    schema= {
+        _id: Joi.number()
+            .required()
+            .label("ID Serial Number"),
+        name: Joi.string()
+            .required()
+            .label("Name"),
+        ipAddress: Joi.string()
+            .required()
+            .label("Crated Date"),
+        devicesList: Joi.number()
+            .required()
+            .min(0)
+            .max(10)
+            .label("Devices Connected")
+    };
+
+    componentDidMount(){
+        const gatewayID = this.props.match.params.id;
+        if (gatewayID === "new") return;
+
+        const gateway = {_id:10, name:"Felipe", ipAddress:"10.8.91.145", devicesList:5};
+        this.setState({data:this.mapToViewModel(gateway)})
+    };
+
+    mapToViewModel=(movie)=>{
+        return {
+            _id:movie._id,
+            name:movie.name,
+            ipAddress:movie.ipAddress,
+            devicesList:movie.devicesList,
+        };
+    };
+
+    doSubmit = ()=>{
+        this.props.history.push("/gateways")
+    };
     render() {
-        const {match} = this.props;
         return (
             <div>
-                <p>Gateways Form {match.params.id}</p>
+                <h1> Gateways Form</h1>
+                <form>
+                    {this.renderInput("_id", "ID Serial Number", "number")}
+                    {this.renderInput("name", "Name")}
+                    {this.renderInput("ipAddress", "IP Address")}
+                    {this.renderInput("devicesList", "ConnectedDevices", "number")}
+                    {this.renderButton('Save')}
+                </form>
             </div>
         );
     }
